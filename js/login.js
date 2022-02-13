851fedd16664da1b530a72f71f7ef2c57e8677a0
masudrana <masudranaacb@gmail.com>
2022-02-13 07:19:31 +0600

    bank balance function

--- js/login.js
+++ js/login.js
@@ -12,15 +12,23 @@ document.getElementById('diposittBtn').addEventListener('click',function(){
     const previusDepositBlence=parseFloat(totalBlance.innerText);
     const newDepositBlence=newDepositAmount+previusDepositBlence;
     totalBlance.innerText=newDepositBlence;
-    
-    
-    
-    
-    
-    
-    
-    
-    
     // clear 
     depositInput.value=''
-})
+});
+// witdrow balance
+const witdrawBtn=document.getElementById('witdrawBtn').addEventListener('click', function(){
+    const withdrawInput=document.getElementById('withdrawInput');
+    const witdrawAmountText=parseFloat(withdrawInput.value);
+    const widrawTotal=document.getElementById('widrawTotal');
+    const previusWidraw=parseFloat(widrawTotal.innerText);
+    const totalWidraw=previusWidraw+witdrawAmountText;
+    widrawTotal.innerText=totalWidraw;
+
+// case balance
+    const totalBlance=document.getElementById('totalBlance');
+    const caseBalance=parseFloat(totalBlance.innerText);
+    const totalCaseBalance=caseBalance-witdrawAmountText;
+    totalBlance.innerText=totalCaseBalance;
+
+    withdrawInput.value=''
+})  
